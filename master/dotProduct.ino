f62c1361398a08698d5498fd607f470dc2d2351d
myleskeller <43857093+myleskeller@users.noreply.github.com>
2019-10-21 19:48:15 -0400

    new matrix parsing method for i2c added

--- master/dotProduct.ino
+++ master/dotProduct.ino
@@ -1,4 +1,4 @@
-uint8_t **A, **B, **C; //A size will be changed
+float **A, **B, **C;
 uint8_t n;
 
 void setupMatrices() {
@@ -11,18 +11,18 @@ void initiateMatrices() {
   n = devices;
   C = malloc(n * sizeof(uint8_t *)); // allocate memory for C
   for (uint8_t i = 0; i < n; i++)
-    C[i] = malloc(n * sizeof(uint8_t));
+    C[i] = malloc(n * sizeof(float));
   A = malloc(n * sizeof(uint8_t *)); // allocate memory for A
   for (uint8_t i = 0; i < n; i++) {
-    A[i] = malloc(n * sizeof(uint8_t));
-    //A[i] = random(1.175494e-38, 3.402823e+38); //i can't figure this out. probably wont make decimals.
-    A[i] = random(1,100);
+    A[i] = malloc(n * sizeof(float));
+    // A[i] = random(1.175494e-38, 3.402823e+38); //i can't figure this out. probably wont make decimals.
+    A[i] = random(1, 100);
   }
   B = malloc(n * sizeof(uint8_t *)); // allocate memory for B
   for (uint8_t i = 0; i < n; i++) {
-    B[i] = malloc(n * sizeof(uint8_t)) = random(1,100);
-    //B[i] = random(1.175494e-38, 3.402823e+38);
- //   B[i] = random(1,100);
+    B[i] = malloc(n * sizeof(float));
+    // B[i] = random(1.175494e-38, 3.402823e+38);
+    B[i] = random(1, 100);
   }
 }
 
@@ -34,40 +34,46 @@ void sendMatrices() {
 
     for (uint8_t i = 0; i < n; i++) {
       for (uint8_t j = 0; j < n; j++) {
-        Serial.print(B[i][j]); Serial.print(' ');
+        Serial.print((byte)&B[i][j]); Serial.print(' ');
       }
       Serial.println();
     }
     Serial.println();
 
     Wire.beginTransmission(0);
-    Wire.write((byte *) &B, sizeof B); //broadcasts array B to all available devices
-    delay(i2c_PROPAGATION_DELAY);
-    Wire.endTransmission();
-/*
-    Serial.println("broadcasting A");
-
     for (uint8_t i = 0; i < n; i++) {
       for (uint8_t j = 0; j < n; j++) {
-        if (i % 4 == 0) {
-          Serial.print("Sending ");
-          Serial.println(i + i2c_ADDRESS_MIN);
-        }
-        Serial.print(A[i - i2c_ADDRESS_MIN][j]); Serial.print(' ');
+        //Wire.write((byte)B[i][j]); //this works fine with hard-coded arrays
+        Wire.write((byte)&B[i][j]);
+        delay(i2c_PROPAGATION_DELAY);
       }
-      Serial.println();
     }
-    Serial.println();
+    Wire.endTransmission();
+    /*
+        Serial.println("broadcasting A");
 
-    for (uint8_t i = i2c_ADDRESS_MIN; i < n + i2c_ADDRESS_MIN; i++ ) {
-      Wire.beginTransmission(i);
-      for (uint8_t x = 0; x < WORKLOAD_MAGNITUDE; x++) {
-        //Wire.write(A[i - i2c_ADDRESS_MIN + x]); //broadcasts partialMatrix to relevant slave
-        Wire.write((byte *) &A[i - i2c_ADDRESS_MIN + x], sizeof A[i - i2c_ADDRESS_MIN + x]); //broadcasts partialMatrix to relevant slave
-      }
-      delay(i2c_PROPAGATION_DELAY);
-      Wire.endTransmission();
-    }
+        for (uint8_t i = 0; i < n; i++) {
+          for (uint8_t j = 0; j < n; j++) {
+            if (i % 4 == 0) {
+              Serial.print("Sending ");
+              Serial.println(i + i2c_ADDRESS_MIN);
+            }
+            Serial.print(A[i - i2c_ADDRESS_MIN][j]); Serial.print(' ');
+          }
+          Serial.println();
+        }
+        Serial.println();
+
+        for (uint8_t i = i2c_ADDRESS_MIN; i < n + i2c_ADDRESS_MIN; i++ ) {
+          Wire.beginTransmission(i);
+          for (uint8_t x = 0; x < WORKLOAD_MAGNITUDE; x++) {
+          //remember, this needs to reflect the appropriate subarray size sent to each slave
+            //Wire.write(A[i - i2c_ADDRESS_MIN + x]); //broadcasts partialMatrix to relevant slave
+            Wire.write((byte *) &A[i - i2c_ADDRESS_MIN + x], sizeof A[i - i2c_ADDRESS_MIN + x]); //broadcasts partialMatrix to relevant slave
+          }
+          delay(i2c_PROPAGATION_DELAY);
+          Wire.endTransmission();
+        }
     */
   }
 }
